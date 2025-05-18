34c6c018377db3d7be202bd69a784b2c2a4eba81
Nusan <lezanu@gmail.com>
2025-05-18 23:52:18 +0200

    Build Script to make an executable

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller randomizer.spec
+copy "dist\item_randomizer.exe" "E:\Jeux\Steam\steamapps\common\Dark Souls Prepare to Die Edition\DATA"
+cd "E:\Jeux\Steam\steamapps\common\Dark Souls Prepare to Die Edition\DATA"
+item_randomizer.exe
+pause
